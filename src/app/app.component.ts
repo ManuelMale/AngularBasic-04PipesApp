d667b4fdcd5d5f19617ed1fef55e60db5745dad1
ManuelMale <mmalerases@gmail.com>
2021-07-02 02:45:24 +0200

    Primer commit de Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+
+  nombre: string = 'Manuel Maleras';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Manuel'
+  }
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }

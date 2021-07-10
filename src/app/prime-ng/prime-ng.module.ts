14fc80d79ffaca7fe549b8a64d59c7de9447be2a
ManuelMale <mmalerases@gmail.com>
2021-07-10 02:18:26 +0200

    Sección 10 - Clase 21 - 24

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 //PrimeNG
 import {CardModule} from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import {FieldsetModule} from 'primeng/fieldset';
 import {MenubarModule} from 'primeng/menubar';
 
 @NgModule({
   exports: [
     ButtonModule,
     CardModule,
+    FieldsetModule,
     MenubarModule
   ]
 })
--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  // i18nSelect
+  nombre: string = 'Susana';
+  genero: string = 'femenino';
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
+  }
 
-  ngOnInit(): void {
+  // i18nPlural
+  clientes: string[] = ['Gparra', 'Pedro', 'Sikey', 'Anremis', 'Ashen'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  cambiarCliente() {
+    this.nombre = 'Melissa';
+    this.genero = 'femenino';
+  }
+
+  borrarCliente() {
+    this.clientes.pop();
   }
 
 }
